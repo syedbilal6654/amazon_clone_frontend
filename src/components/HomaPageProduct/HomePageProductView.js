662d8e46a95f4a65fe4c116300836d087e44378a
Syed Bilal Ali <sy54bilal@gmail.com>
2022-12-01 18:38:27 +0530

    HomaPageView

---
+++ src/components/HomaPageProduct/HomePageProductView.js
@@ -0,0 +1,302 @@
+import React from 'react'
+
+function HomePageProductView() {
+  return (
+   <React.Fragment>
+   
+   <div className="container mb-5" style={{marginTop:"-250px"}}>
+
+
+   <div className="row " >
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images1.jpg" alt="productimage1"/>
+
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               150.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images2.jpg" alt="productimage2"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               700.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images3.jpg" alt="productimage3"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               260.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images4.jpg" alt="productimage4"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               250.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+
+
+   <div className="row my-0 my-md-2">
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images1.jpg" alt="productimage1"/>
+
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               150.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images2.jpg" alt="productimage2"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               700.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images3.jpg" alt="productimage3"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               260.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images4.jpg" alt="productimage4"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               250.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+
+   <div className="row my-0 my-md-2">
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images1.jpg" alt="productimage1"/>
+
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               150.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images2.jpg" alt="productimage2"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               700.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images3.jpg" alt="productimage3"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               260.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div className="col-md-3">
+           <div className="card my-2 my-md-0">
+               <div className="card-body d-flex justify-content-center">
+                   <a href="#">
+                       <img src="asserts/img/home_page_images/images4.jpg" alt="productimage4"/>
+                   </a>
+               </div>
+               <div className="card-footer py-0">
+                   <div className="row">
+                       <div className="col-md-6">
+                           <p className="font-weight-bold text-danger my-0 "><i
+                                   className="fa-sharp fa-solid fa-indian-rupee-sign"></i>
+                               250.00
+                           </p>
+
+                       </div>
+                       <div className="col-md-6">
+                           <a href="#" className="btn btn-success btn-sm btn-block ">Buy</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+   </React.Fragment>
+  )
+}
+export default HomePageProductView;
--- src/components/Layout/Landingpage.js
+++ src/components/Layout/Landingpage.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import NavbarContainer from './NavbarConatiner/Navbar'
 import ImageSlider from './ImageSlider'
-
+import HomePageProductContainer from '../HomaPageProduct/HomePageProductContainer'
  function Landingpage() {
   return (
     <React.Fragment>
     <NavbarContainer/>
     <div className='container-fluid px-0'>
     <ImageSlider/>
+    <HomePageProductContainer/>
     </div>
     </React.Fragment>
   )
